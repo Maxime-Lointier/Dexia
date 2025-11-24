0907824b247c8f99b4e720d462c5d7e3ecf9e55e
DenisVuong <164088452+DenisVuong@users.noreply.github.com>
2025-11-24 23:07:34 +0100

    changement index + mise à O onboarding, ajout page

---
+++ app/settings.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Page Settings</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
