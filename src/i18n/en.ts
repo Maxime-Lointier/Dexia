c7e56381bae99fc12ed85f298d939399f6b19894
etoile <fabio.etoile52@gmail.com>
2026-01-18 20:03:16 +0100

    onboarding et welcome traduit

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -34,11 +34,39 @@ export default {
       terms: "Terms of service",
       version: "Version",
     },
-
+  },
     welcome: {
       subtitle : "The Right Swipe",
       description :"Discover movies that truly match you in just a few swipes.",
       startButton : "Start the adventure",
-    }
+    },
+
+    onboarding: {
+      title: "Your favorite genres",
+      subtitle: "Select at least 3 genres",
+      loading: "Loading...",
+      continue: "Continue",
+      genres: {
+        28: "Action",
+        12: "Adventure",
+        16: "Animation",
+        35: "Comedy",
+        80: "Crime",
+        99: "Documentary",
+        18: "Drama",
+        10751: "Family",
+        14: "Fantasy",
+        36: "History",
+        27: "Horror",
+        10402: "Music",
+        9648: "Mystery",
+        10749: "Romance",
+        878: "Sci-Fi",
+        53: "Thriller",
+        10752: "War",
+        37: "Western",
+        10770: "Tv Movie"
+      }
   },
+
 };
--- src/i18n/fr.ts
+++ src/i18n/fr.ts
@@ -1,6 +1,6 @@
 export default {
   settings: {
-    title: "Paramètres",
+    title: "Paramètres",  
 
     language: {
       sectionTitle: "Langue",
@@ -34,11 +34,39 @@ export default {
       terms: "Conditions d'utilisation",
       version: "Version",
     },
-
+  },
     welcome: {
       subtitle: "The Right Swipe",
       description :"Découvre des films qui te correspondent vraiment en quelques swipes.",
       startButton: "Commencer l'aventure",
-    }
-  },
+    },
+onboarding: {
+  title: "Vos genres préférés",
+  subtitle: "Sélectionnez au moins 3 genres",
+  loading: "Chargement...",
+  continue: "Continuer",
+  genres: {
+    28: "Action",
+    12: "Aventure",
+    16: "Animation",
+    35: "Comédie",
+    80: "Crime",
+    99: "Documentaire",
+    18: "Drame",
+    10751: "Famille",
+    14: "Fantastique",
+    36: "Histoire",
+    27: "Horreur",
+    10402: "Musique",
+    9648: "Mystère",
+    10749: "Romance",
+    878: "Sci-Fi",
+    53: "Thriller",
+    10752: "Guerre",
+    37: "Western",
+    10770 : "Téléfilm",
+
+  }
+},    
+
 };
